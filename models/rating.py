3eaa63df5056a7b34a40eda56984a77dfa36b738
bimo <bimo@portcities.net>
2019-10-03 11:33:34 +0700

    [ADD] add new menu validation

--- models/rating.py
+++ models/rating.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import fields, models,api
 
 class PersebayaPerfomance(models.Model):
 	_name = 'persebaya.rating'
@@ -8,3 +8,16 @@ class PersebayaPerfomance(models.Model):
 	jadwal_id = fields.Many2one('persebaya.jadwal',string="Schedule",readonly=True)
 	rating = fields.Integer(string="Rating")
 	review = fields.Char(string="Review")
+
+	@api.multi
+	def action_remove_review(self):
+		self.write({'review':''})
+
+	@api.multi
+	def name_get(self):
+		res = []
+		for s in self:
+			nama = s.employee_id.name or ''
+			res.append((s.id, nama))
+
+		return res
